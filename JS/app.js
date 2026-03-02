f7f1394a751280ba6a4973090ecc27199d8dac50
Daniel Rebolledo R <dreyesrebolledo@gmail.com>
2026-03-02 18:14:10 -0300

    correccion JS

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,27 @@
 window.onload = function () {
 
   const params = new URLSearchParams(window.location.search);
-  const producto = params.get("producto");
+  let producto = params.get("producto");
+
+  console.log("Producto recibido:", producto);
+
+  if (!producto) {
+    producto = "lechuga";
+  }
+
+  producto = producto.toLowerCase();
 
   const modelo = document.getElementById("modelo");
 
   const modelosDisponibles = {
     lechuga: "models/Lechuga.glb",
     plato: "models/mesa.glb",
-
   };
 
   if (modelosDisponibles[producto]) {
     modelo.setAttribute("gltf-model", modelosDisponibles[producto]);
   } else {
-    modelo.setAttribute("gltf-model", "models/Lechuga.glb"); // default
+    modelo.setAttribute("gltf-model", "models/Lechuga.glb");
   }
 
 };
