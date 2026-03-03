3e885331259a2606280bc53e366ffb93eb8a78d4
Daniel Rebolledo R <dreyesrebolledo@gmail.com>
2026-03-03 12:27:34 -0300

    cambios en html y modelos llamada

--- JS/app.js
+++ JS/app.js
@@ -1,27 +1,20 @@
-window.onload = function () {
+document.querySelector("a-scene").addEventListener("loaded", function () {
 
   const params = new URLSearchParams(window.location.search);
   let producto = params.get("producto");
 
-  console.log("Producto recibido:", producto);
-
-  if (!producto) {
-    producto = "lechuga";
-  }
-
+  if (!producto) producto = "lechuga";
   producto = producto.toLowerCase();
 
-  const modelo = document.getElementById("modelo");
-
   const modelosDisponibles = {
-    lechuga: "models/Lechuga.glb",
-    plato: "models/plato_nuevo.glb",
+    lechuga: "assets/Lechuga.glb",
+    plato: "assets/plato_nuevo.glb",
   };
 
-  if (modelosDisponibles[producto]) {
+  const modelo = document.querySelector("#modelo");
+
+  if (modelo && modelosDisponibles[producto]) {
     modelo.setAttribute("gltf-model", modelosDisponibles[producto]);
-  } else {
-    modelo.setAttribute("gltf-model", "models/Lechuga.glb");
   }
 
-};
+});
